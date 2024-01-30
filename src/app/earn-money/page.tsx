9a06354f9646c050ead502e3541dbe9dcb6a2087
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2024-01-30 14:59:29 +0500

    EarnMoney

---
+++ src/app/earn-money/page.tsx
@@ -0,0 +1,10 @@
+import Banner from '@/components/earnmoney/banner'
+import React from 'react'
+
+export default function EarnMoney() {
+    return (
+        <>
+            <Banner />
+        </>
+    )
+}
