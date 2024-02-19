ed1de65a73510cebc72b8b8b232d1ae0843e3ef0
sued-zunair-gillani <zunairgillani54@gmail.com>
2024-02-19 19:19:53 +0500

    update

--- src/app/api/add-survey-to-user/route.ts
+++ src/app/api/add-survey-to-user/route.ts
@@ -2,35 +2,42 @@ import { NextResponse } from "next/server";
 import { client } from "../../../../sanity/lib/client";
 
 export async function POST(req: Request) {
-  try {
-    const { ids, _id } =
-      (await req.json()) as {
-          ids: any;
-          _id : string
-      };
+  // try {
+  //   const { ids, _id } =
+  //     (await req.json()) as {
+  //         ids: any;
+  //         _id : string
+  //     };
 
-    // update User
-    const result = client.patch(_id).set({
-     fname: ids
-    })
+  //   // update User
+  //   const result = client.patch(_id).set({
+  //    fname: ids
+  //   })
 
 
-    if (result) {
-      return new NextResponse(
-        JSON.stringify({
-          message: "User Updated | Survey Added",
-        }),
-        { status: 201 }
-      );
-    }
+  //   if (result) {
+  //     return new NextResponse(
+  //       JSON.stringify({
+  //         message: "User Updated | Survey Added",
+  //       }),
+  //       { status: 201 }
+  //     );
+  //   }
 
-  } catch (error: any) {
-    return new NextResponse(
-      JSON.stringify({
-        status: "error",
-        message: error.message,
-      }),
-      { status: 500 }
-    );
-  }
+  // } catch (error: any) {
+  //   return new NextResponse(
+  //     JSON.stringify({
+  //       status: "error",
+  //       message: error.message,
+  //     }),
+  //     { status: 500 }
+  //   );
+  // }
+
+  return new NextResponse(
+    JSON.stringify({
+      message: "Success",
+    }),
+    { status: 200 }
+  );
 }
