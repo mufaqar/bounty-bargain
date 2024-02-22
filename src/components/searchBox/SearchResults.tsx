18310b880cf4301b5265b681fbe659d7f8bb070f
sued-zunair-gillani <zunairgillani54@gmail.com>
2024-02-22 17:19:30 +0500

    search

---
+++ src/components/searchBox/SearchResults.tsx
@@ -0,0 +1,72 @@
+'use client'
+import { GlobalContext } from '@/context/global-context'
+import { useRouter } from 'next/navigation'
+import React, { useContext, useEffect, useState } from 'react'
+
+
+const SearchResults = ({ input }: any) => {
+     const [coupons, setCoupon] = useState<any>()
+     const [categories, setCategories] = useState<any>()
+     const { openModal, setCopan } = useContext(GlobalContext)
+     const router = useRouter()
+
+     useEffect(() => {
+          (async () => {
+               const res = await fetch(`${process.env.NEXT_PUBLIC_FRONEND_URL}api/coupons`)
+               const CatRes = await fetch(`${process.env.NEXT_PUBLIC_FRONEND_URL}/api/categories`)
+
+               if (!res.ok) {
+                    throw new Error('Failed to fetch data')
+               }
+               const categoriesRes = await CatRes.json()
+               const couponsRes = await res.json()
+
+               const coupons = couponsRes?.data?.filter((item: any) => item.name?.toLowerCase()?.includes(input.toLowerCase()) || item.info?.toLowerCase()?.includes(input?.toLowerCase()))
+               const categories = categoriesRes?.data?.filter((item: any) => item.name?.toLowerCase()?.includes(input.toLowerCase()) || item.info?.toLowerCase()?.includes(input?.toLowerCase()))
+               // setCategories()
+               setCoupon(coupons)
+               setCategories(categories)
+          })()
+     }, [input])
+
+     return (
+          <div className='absolute top-12 rounded-lg shadow-xl p-3 bg-[#F7F9FF] w-full'>
+               {
+                    coupons?.length > 0 && <div>
+                         <h2 className='font-medium text-primary mb-1 px-2'>Coupons / Deals</h2>
+                         <ul>
+                              {
+                                   coupons?.slice(0, 5).map((item: any, idx: number) => (
+                                        <li key={idx} className='text-sm p-[1px] capitalize px-2 hover:text-primary cursor-pointer' onClick={() => {
+                                             setCopan(item);
+                                             openModal('coupon')
+                                        }}>
+                                             {item.name}
+                                        </li>
+                                   ))
+                              }
+                         </ul>
+                    </div>
+               }
+               {
+                    categories?.length > 0 && <div>
+                         <h2 className='font-medium text-primary my-1 px-2'>Categories</h2>
+                         <ul>
+                              {
+                                   categories?.slice(0, 5).map((item: any, idx: number) => (
+                                        <li key={idx} className='text-sm p-[1px] px-2 capitalize hover:text-primary cursor-pointer' onClick={() => {
+                                             router.push(`/category/${item.slug.current}`)
+                                        }}>
+                                             {item.name}
+                                        </li>
+                                   ))
+                              }
+                         </ul>
+                    </div>
+               }
+
+          </div>
+     )
+}
+
+export default SearchResults
--- src/components/searchBox/searchBox.tsx
+++ src/components/searchBox/searchBox.tsx
@@ -4,21 +4,28 @@ import { FiSearch } from "react-icons/fi";
 import Button from '../UI/button/button';
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { useRouter } from 'next/navigation';
+import SearchResults from './SearchResults';
 
 const SearchBox = () => {
      const router = useRouter()
      const [input, setInput] = useState('')
      const handleSearch = () => {
-          router.push(`/deals?q=${input}`)
+          router.push(`/submit-coupon`)
      }
+     
 
      return (
           <section className='max-w-[650px] px-2 md:px-0 mx-auto py-4 md:py-8'>
                <div className='flex md:flex-row flex-col gap-3'>
-                    <div className='border flex items-center p-2.5 flex-1 rounded-md text-neutral gap-2'>
-                         <FiSearch className='text-xl'/>
-                         <input type='text' value={input} onChange={(e)=>setInput(e.target.value)} className='placeholder:text-light outline-none w-full' placeholder='What are you looking for?' />
+                    <div className="flex-1 relative ">
+                         <div className='border flex items-center p-2.5 rounded-md text-neutral gap-2'>
+                              <FiSearch className='text-xl' />
+                              <input type='text' value={input} onChange={(e) => setInput(e.target.value)} className='placeholder:text-light outline-none w-full' placeholder='What are you looking for?' />
+                         </div>
+                         { input && <SearchResults input={input}/> }
+                         
                     </div>
+
                     <Button
                          size="large"
                          color='secondary'
@@ -26,8 +33,8 @@ const SearchBox = () => {
                          className='!text-sm'
                          click={handleSearch}
                     >
-                          <AiOutlinePlusCircle/>
-                          Submit Coupon/Deal
+                         <AiOutlinePlusCircle />
+                         Submit Coupon/Deal
                     </Button>
                </div>
           </section>
