4768161c7f13f3b674462591fe863d9bf764c95a
sued-zunair-gillani <zunairgillani54@gmail.com>
2024-03-08 19:10:09 +0500

    Add initial icon as author image placeholder

--- src/components/blog/post-box.tsx
+++ src/components/blog/post-box.tsx
@@ -16,15 +16,20 @@ const PostBox = ({ data }: any) => {
                     {data?._createdAt}
                 </p>
 
-                <Link href={`/blog/${data?.slug?.current}`} className='text-base font-bold text-dark'>
+                <Link href={`/blog/${data?.slug?.current}`} className='text-base font-bold text-dark line-clamp-1'>
                     {data?.title}
                 </Link>
                 <div className='flex items-center gap-5 pt-6'>
                     <div className='w-[41px] h-[41px] rounded-full'>
-                        <Image src={data?.writtenby?.profile_image?.asset?.url ? data?.writtenby?.profile_image?.asset?.url : "/images/feature.png"} alt='feature' width={336} height={203} className='w-full h-full rounded-full' />
+                        {
+                            data?.writtenby?.profile_image?.asset?.url ?
+                            <Image src={data?.writtenby?.profile_image?.asset?.url} alt='feature' width={336} height={203} className='w-full h-full rounded-full' /> :
+                            <div className="bg-gray-200 flex justify-center items-center flex-col text-sm w-[41px] h-[41px] uppercase rounded-full font-medium">{data?.writtenby?.fname.charAt(0)}{data?.writtenby?.fname.charAt(1)}</div>
+                        }
+                        
                     </div>
                     <div>
-                        <h4 className='text-sm font-semibold text-dark'>
+                        <h4 className='text-sm font-semibold text-dark '>
                         {data?.writtenby?.fname} {data?.writtenby?.lname}
                         </h4>
                         <p className='text-xs font-normal text-neutral/80'>
